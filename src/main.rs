36efadb357ac0432a92775b45fc6ea4f33b87918
Kirill Nikitin <locke23rus@gmail.com>
2023-06-25 01:52:43 +0300

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod registries;
 mod routes;
 mod templates;
 
-use axum::{handler::Handler, routing::get, Router};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -12,7 +12,7 @@ async fn main() {
 		.route("/", get(routes::index))
 		.route("/inspect/:registry/:pkg/:version", get(routes::inspect))
 		.route("/compare/:registry/:pkg/:v1/:v2", get(routes::compare))
-		.fallback(routes::handler_404.into_service());
+		.fallback(routes::handler_404);
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 	axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
 }
